53c0fefc548a49c8f58db8b3800c75f1b96ba2d5
Geoff Lamrock <geoff.lamrock@gmail.com>
2024-11-06 08:08:40 +1100

    Add --all to stack status

--- src/Stack/Commands/Stack/StackStatusCommand.cs
+++ src/Stack/Commands/Stack/StackStatusCommand.cs
@@ -12,12 +12,18 @@ internal class StackStatusCommandSettings : CommandSettingsBase
     [Description("The name of the stack to show the status of.")]
     [CommandOption("-n|--name")]
     public string? Name { get; init; }
+
+    [Description("Show status of all stacks.")]
+    [CommandOption("--all")]
+    public bool All { get; init; }
 }
 
 record BranchStatus(bool ExistsInRemote, int Ahead, int Behind);
 
 internal class StackStatusCommand : AsyncCommand<StackStatusCommandSettings>
 {
+    record StackStatus(Dictionary<string, BranchStatus> BranchStatuses, Dictionary<string, GitHubPullRequest> PullRequests);
+
     public override async Task<int> ExecuteAsync(CommandContext context, StackStatusCommandSettings settings)
     {
         await Task.CompletedTask;
@@ -41,39 +47,50 @@ internal class StackStatusCommand : AsyncCommand<StackStatusCommandSettings>
 
         var currentBranch = GitOperations.GetCurrentBranch(settings.GetGitOperationSettings());
 
-        var stackSelection = settings.Name ?? AnsiConsole.Prompt(Prompts.Stack(stacksForRemote, currentBranch));
-        var stack = stacksForRemote.First(s => s.Name.Equals(stackSelection, StringComparison.OrdinalIgnoreCase));
+        var stacksToCheckStatusFor = new Dictionary<Config.Stack, StackStatus>();
 
-        var allBranchesInStack = new List<string>([stack.SourceBranch]).Concat(stack.Branches).Distinct().ToArray();
-        var branchesThatExistInRemote = GitOperations.GetBranchesThatExistInRemote(allBranchesInStack, settings.GetGitOperationSettings());
-
-        var remoteStatusForBranchesInStacks = new Dictionary<string, BranchStatus>();
-        var prForBranchesInStacks = new Dictionary<string, GitHubPullRequest>();
+        if (settings.All)
+        {
+            stacksForRemote.ForEach(stack => stacksToCheckStatusFor.Add(stack, new StackStatus([], [])));
+        }
+        else
+        {
+            var stackSelection = settings.Name ?? AnsiConsole.Prompt(Prompts.Stack(stacksForRemote, currentBranch));
+            var stack = stacksForRemote.First(s => s.Name.Equals(stackSelection, StringComparison.OrdinalIgnoreCase));
+            stacksToCheckStatusFor.Add(stack, new StackStatus([], []));
+        }
 
         AnsiConsole.Status()
             .Start("Checking status of remote branches...", ctx =>
             {
-                GitOperations.FetchBranches(branchesThatExistInRemote, settings.GetGitOperationSettings());
-
-                void CheckRemoteBranch(string branch, string sourceBranch)
+                foreach (var (stack, status) in stacksToCheckStatusFor
+                )
                 {
-                    var (ahead, behind) = GitOperations.GetStatusOfRemoteBranch(branch, sourceBranch, settings.GetGitOperationSettings());
-                    var branchStatus = new BranchStatus(true, ahead, behind);
-                    remoteStatusForBranchesInStacks[branch] = branchStatus;
-                }
+                    var allBranchesInStack = new List<string>([stack.SourceBranch]).Concat(stack.Branches).Distinct().ToArray();
+                    var branchesThatExistInRemote = GitOperations.GetBranchesThatExistInRemote(allBranchesInStack, settings.GetGitOperationSettings());
 
-                var parentBranch = stack.SourceBranch;
+                    GitOperations.FetchBranches(branchesThatExistInRemote, settings.GetGitOperationSettings());
 
-                foreach (var branch in stack.Branches)
-                {
-                    if (branchesThatExistInRemote.Contains(branch))
+                    void CheckRemoteBranch(string branch, string sourceBranch)
                     {
-                        CheckRemoteBranch(branch, parentBranch);
-                        parentBranch = branch;
+                        var (ahead, behind) = GitOperations.GetStatusOfRemoteBranch(branch, sourceBranch, settings.GetGitOperationSettings());
+                        var branchStatus = new BranchStatus(true, ahead, behind);
+                        status.BranchStatuses[branch] = branchStatus;
                     }
-                    else
+
+                    var parentBranch = stack.SourceBranch;
+
+                    foreach (var branch in stack.Branches)
                     {
-                        remoteStatusForBranchesInStacks[branch] = new BranchStatus(false, 0, 0);
+                        if (branchesThatExistInRemote.Contains(branch))
+                        {
+                            CheckRemoteBranch(branch, parentBranch);
+                            parentBranch = branch;
+                        }
+                        else
+                        {
+                            status.BranchStatuses[branch] = new BranchStatus(false, 0, 0);
+                        }
                     }
                 }
             });
@@ -81,94 +98,100 @@ internal class StackStatusCommand : AsyncCommand<StackStatusCommandSettings>
         AnsiConsole.Status()
             .Start("Checking status of GitHub pull requests...", ctx =>
             {
-                try
+                foreach (var (stack, status) in stacksToCheckStatusFor)
                 {
-                    foreach (var branch in stack.Branches)
+                    try
                     {
-                        var pr = GitHubOperations.GetPullRequest(branch, settings.GetGitHubOperationSettings());
-
-                        if (pr is not null)
+                        foreach (var branch in stack.Branches)
                         {
-                            prForBranchesInStacks[branch] = pr;
+                            var pr = GitHubOperations.GetPullRequest(branch, settings.GetGitHubOperationSettings());
+
+                            if (pr is not null)
+                            {
+                                status.PullRequests[branch] = pr;
+                            }
                         }
                     }
-                }
-                catch (Exception ex)
-                {
-                    AnsiConsole.MarkupLine($"[orange1]Error checking GitHub pull requests: {ex.Message}[/]");
+                    catch (Exception ex)
+                    {
+                        AnsiConsole.MarkupLine($"[orange1]Error checking GitHub pull requests: {ex.Message}[/]");
+                    }
                 }
             });
 
-        var stackRoot = new Tree($"[yellow]{stack.Name}:[/] [grey]{stack.SourceBranch}[/]");
-
-        string BuildBranchName(string branch, string? parentBranch, bool isSourceBranchForStack)
+        foreach (var (stack, status) in stacksToCheckStatusFor)
         {
-            var status = remoteStatusForBranchesInStacks.GetValueOrDefault(branch);
-            var branchNameBuilder = new StringBuilder();
+            var stackRoot = new Tree($"[yellow]{stack.Name}:[/] [grey]{stack.SourceBranch}[/]");
 
-            var color = status?.ExistsInRemote == false ? "grey" : isSourceBranchForStack ? "grey" : branch.Equals(currentBranch, StringComparison.OrdinalIgnoreCase) ? "blue" : null;
-            Decoration? decoration = status?.ExistsInRemote == false ? Decoration.Strikethrough : null;
+            string BuildBranchName(string branch, string? parentBranch, bool isSourceBranchForStack)
+            {
+                var barnchStatus = status.BranchStatuses.GetValueOrDefault(branch);
+                var branchNameBuilder = new StringBuilder();
 
-            if (color is not null && decoration is not null)
-            {
-                branchNameBuilder.Append($"[{decoration} {color}]{branch}[/]");
-            }
-            else if (color is not null)
-            {
-                branchNameBuilder.Append($"[{color}]{branch}[/]");
-            }
-            else if (decoration is not null)
-            {
-                branchNameBuilder.Append($"[{decoration}]{branch}[/]");
-            }
-            else
-            {
-                branchNameBuilder.Append(branch);
-            }
+                var color = barnchStatus?.ExistsInRemote == false ? "grey" : isSourceBranchForStack ? "grey" : branch.Equals(currentBranch, StringComparison.OrdinalIgnoreCase) ? "blue" : null;
+                Decoration? decoration = barnchStatus?.ExistsInRemote == false ? Decoration.Strikethrough : null;
 
-            if (status?.Ahead > 0 && status?.Behind > 0)
-            {
-                branchNameBuilder.Append($" [grey]({status.Ahead} ahead, {status.Behind} behind {parentBranch})[/]");
-            }
-            else if (status?.Ahead > 0)
-            {
-                branchNameBuilder.Append($" [grey]({status.Ahead} ahead of {parentBranch})[/]");
-            }
-            else if (status?.Behind > 0)
-            {
-                branchNameBuilder.Append($" [grey]({status.Behind} behind {parentBranch})[/]");
-            }
-
-            if (prForBranchesInStacks.TryGetValue(branch, out var pr))
-            {
-                var prStatusColor = Color.Green;
-                if (pr.State == GitHubPullRequestStates.Merged)
+                if (color is not null && decoration is not null)
                 {
-                    prStatusColor = Color.Purple;
+                    branchNameBuilder.Append($"[{decoration} {color}]{branch}[/]");
                 }
-                else if (pr.State == GitHubPullRequestStates.Closed)
+                else if (color is not null)
                 {
-                    prStatusColor = Color.Red;
+                    branchNameBuilder.Append($"[{color}]{branch}[/]");
                 }
-                branchNameBuilder.Append($" [{prStatusColor} link={pr.Url}]#{pr.Number}: {pr.Title}[/]");
+                else if (decoration is not null)
+                {
+                    branchNameBuilder.Append($"[{decoration}]{branch}[/]");
+                }
+                else
+                {
+                    branchNameBuilder.Append(branch);
+                }
+
+                if (barnchStatus?.Ahead > 0 && barnchStatus?.Behind > 0)
+                {
+                    branchNameBuilder.Append($" [grey]({barnchStatus.Ahead} ahead, {barnchStatus.Behind} behind {parentBranch})[/]");
+                }
+                else if (barnchStatus?.Ahead > 0)
+                {
+                    branchNameBuilder.Append($" [grey]({barnchStatus.Ahead} ahead of {parentBranch})[/]");
+                }
+                else if (barnchStatus?.Behind > 0)
+                {
+                    branchNameBuilder.Append($" [grey]({barnchStatus.Behind} behind {parentBranch})[/]");
+                }
+
+                if (status.PullRequests.TryGetValue(branch, out var pr))
+                {
+                    var prStatusColor = Color.Green;
+                    if (pr.State == GitHubPullRequestStates.Merged)
+                    {
+                        prStatusColor = Color.Purple;
+                    }
+                    else if (pr.State == GitHubPullRequestStates.Closed)
+                    {
+                        prStatusColor = Color.Red;
+                    }
+                    branchNameBuilder.Append($" [{prStatusColor} link={pr.Url}]#{pr.Number}: {pr.Title}[/]");
+                }
+
+                return branchNameBuilder.ToString();
             }
 
-            return branchNameBuilder.ToString();
-        }
+            string parentBranch = stack.SourceBranch;
 
-        string parentBranch = stack.SourceBranch;
-
-        foreach (var branch in stack.Branches)
-        {
-            stackRoot.AddNode(BuildBranchName(branch, parentBranch, false));
-
-            if (remoteStatusForBranchesInStacks.TryGetValue(branch, out var status) && status.ExistsInRemote)
+            foreach (var branch in stack.Branches)
             {
-                parentBranch = branch;
-            }
-        }
+                stackRoot.AddNode(BuildBranchName(branch, parentBranch, false));
 
-        AnsiConsole.Write(stackRoot);
+                if (status.BranchStatuses.TryGetValue(branch, out var branchStatus) && branchStatus.ExistsInRemote)
+                {
+                    parentBranch = branch;
+                }
+            }
+
+            AnsiConsole.Write(stackRoot);
+        }
 
         return 0;
     }
