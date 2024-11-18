7cfd0c676738edf1815d9046645f257c937e354b
Geoff Lamrock <geoff.lamrock@gmail.com>
2024-11-18 19:55:19 +1100

    Less chance of collision in tests

--- src/Stack.Tests/Helpers/Some.cs
+++ src/Stack.Tests/Helpers/Some.cs
@@ -4,6 +4,6 @@ namespace Stack.Tests.Helpers;
 
 public static class Some
 {
-    public static string Name() => Guid.NewGuid().ToString("N")[..8];
+    public static string Name() => Guid.NewGuid().ToString("N");
     public static Uri HttpsUri() => new($"https://{Name()}.com");
 }
