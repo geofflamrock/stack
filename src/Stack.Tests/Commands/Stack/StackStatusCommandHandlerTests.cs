854562a6195df8fffadb146cad2ed5b7bc755d5e
Geoff Lamrock <geoff.lamrock@gmail.com>
2024-12-09 17:33:54 +1100

    Fix issue where `status --all` would return stacks for different repositories

--- src/Stack.Tests/Commands/Stack/StackStatusCommandHandlerTests.cs
+++ src/Stack.Tests/Commands/Stack/StackStatusCommandHandlerTests.cs
@@ -218,6 +218,82 @@ public class StackStatusCommandHandlerTests
     }
 
     [Fact]
+    public async Task WhenAllStacksAreRequested_WithStacksInMultipleRepositories_ReturnsStatusOfEachStackInTheCorrectRepository()
+    {
+        // Arrange
+        var gitOperations = Substitute.For<IGitOperations>();
+        var gitHubOperations = Substitute.For<IGitHubOperations>();
+        var stackConfig = Substitute.For<IStackConfig>();
+        var inputProvider = Substitute.For<IInputProvider>();
+        var outputProvider = Substitute.For<IOutputProvider>();
+        var handler = new StackStatusCommandHandler(inputProvider, outputProvider, gitOperations, gitHubOperations, stackConfig);
+
+        var remoteUri = Some.HttpsUri().ToString();
+
+        gitOperations.GetRemoteUri().Returns(remoteUri);
+        gitOperations.GetCurrentBranch().Returns("branch-1");
+
+        var stack1 = new Config.Stack("Stack1", remoteUri, "branch-1", ["branch-3", "branch-5"]);
+        var stack2 = new Config.Stack("Stack2", remoteUri, "branch-2", ["branch-4"]);
+        var stack3 = new Config.Stack("Stack2", Some.HttpsUri().ToString(), "branch-6", ["branch-7"]);
+        var stacks = new List<Config.Stack>([stack1, stack2]);
+        stackConfig.Load().Returns(stacks);
+
+        outputProvider
+            .WhenForAnyArgs(o => o.Status(Arg.Any<string>(), Arg.Any<Action>()))
+            .Do(ci => ci.ArgAt<Action>(1)());
+
+        gitOperations
+            .GetBranchesThatExistInRemote(Arg.Any<string[]>())
+            .Returns(["branch-1", "branch-2", "branch-3", "branch-4", "branch-5"]);
+
+        gitOperations
+            .GetBranchesThatExistLocally(Arg.Any<string[]>())
+            .Returns(["branch-1", "branch-2", "branch-3", "branch-4", "branch-5"]);
+
+        gitOperations
+            .GetStatusOfRemoteBranch("branch-3", "branch-1")
+            .Returns((10, 5));
+
+        gitOperations
+            .GetStatusOfRemoteBranch("branch-5", "branch-3")
+            .Returns((1, 0));
+
+        gitOperations
+            .GetStatusOfRemoteBranch("branch-4", "branch-2")
+            .Returns((3, 1));
+
+        var pr = new GitHubPullRequest(1, "PR title", "PR body", GitHubPullRequestStates.Open, Some.HttpsUri());
+
+        gitHubOperations
+            .GetPullRequest("branch-3")
+            .Returns(pr);
+
+        // Act
+        var response = await handler.Handle(new StackStatusCommandInputs(null, true));
+
+        // Assert
+        var expectedBranchDetailsForStack1 = new Dictionary<string, BranchDetail>
+        {
+            { "branch-3", new BranchDetail { Status = new BranchStatus(true, true, 10, 5), PullRequest = pr } },
+            { "branch-5", new BranchDetail { Status = new BranchStatus(true, true, 1, 0) } }
+        };
+        var expectedBranchDetailsForStack2 = new Dictionary<string, BranchDetail>
+        {
+            { "branch-4", new BranchDetail { Status = new BranchStatus(true, true, 3, 1) } }
+        };
+        response.Statuses.Should().BeEquivalentTo(new Dictionary<Config.Stack, StackStatus>
+        {
+            {
+                stack1, new(expectedBranchDetailsForStack1)
+            },
+            {
+                stack2, new(expectedBranchDetailsForStack2)
+            }
+        });
+    }
+
+    [Fact]
     public async Task WhenStackNameIsProvided_ButStackDoesNotExist_Throws()
     {
         // Arrange
