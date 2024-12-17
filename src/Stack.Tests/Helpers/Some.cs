29d4f086c69597ceffc9b9c9562c3ac24e5cfe93
Geoff Lamrock <geoff.lamrock@gmail.com>
2024-12-18 08:44:43 +1100

    Use proper Git repository for add branch tests (#144)

--- src/Stack.Tests/Helpers/Some.cs
+++ src/Stack.Tests/Helpers/Some.cs
@@ -5,5 +5,7 @@ namespace Stack.Tests.Helpers;
 public static class Some
 {
     public static string Name() => Guid.NewGuid().ToString("N");
-    public static Uri HttpsUri() => new($"https://{Name()}.com");
+    public static string ShortName() => Guid.NewGuid().ToString("N").Substring(0, 8);
+    public static string BranchName() => $"branch-{ShortName()}";
+    public static Uri HttpsUri() => new($"https://{ShortName()}.com");
 }
---
+++ src/Stack.Tests/Helpers/TestGitRepositoryBuilder.cs
@@ -0,0 +1,88 @@
+using System;
+using LibGit2Sharp;
+using Microsoft.VisualBasic;
+using Stack.Git;
+
+namespace Stack.Tests.Helpers;
+
+public class TestGitRepositoryBuilder
+{
+    List<string> branches = [];
+
+    public TestGitRepositoryBuilder WithBranch(string branch)
+    {
+        branches.Add(branch);
+        return this;
+    }
+
+    public TestGitRepository Build()
+    {
+        var remote = Path.Join(Path.GetTempPath(), Guid.NewGuid().ToString("N"), ".git");
+        var remoteDirectory = new TemporaryDirectory(remote);
+        var localDirectory = TemporaryDirectory.Create();
+
+        Repository.Init(remoteDirectory.DirectoryPath, true);
+        var repo = new Repository(Repository.Clone(remote, localDirectory.DirectoryPath));
+        var defaultBranch = Some.BranchName();
+        repo.Refs.UpdateTarget("HEAD", "refs/heads/" + defaultBranch);
+
+        Commit(repo, ("README.md", "Hello, World!"));
+
+        foreach (var branch in branches)
+        {
+            repo.CreateBranch(branch);
+        }
+
+        return new TestGitRepository(localDirectory, remoteDirectory, repo);
+    }
+
+    private Commit Commit(Repository repository, params (string Name, string? Content)[] files)
+    {
+        var message = $"Commit: {Some.Name()}";
+        var signature = new Signature(Some.Name(), Some.Name(), DateTimeOffset.Now);
+
+        return repository.Commit(message, signature, signature);
+    }
+}
+
+public class TestGitRepository(TemporaryDirectory LocalDirectory, TemporaryDirectory RemoteDirectory, Repository Repository) : IDisposable
+{
+    public TemporaryDirectory LocalDirectory { get; } = LocalDirectory;
+    public TemporaryDirectory RemoteDirectory { get; } = RemoteDirectory;
+    public string RemoteUri => RemoteDirectory.DirectoryPath;
+    public GitOperationSettings GitOperationSettings => new GitOperationSettings(false, false, LocalDirectory.DirectoryPath);
+
+    public void Dispose()
+    {
+        GC.SuppressFinalize(this);
+        Repository.Dispose();
+        LocalDirectory.Dispose();
+        RemoteDirectory.Dispose();
+    }
+}
+
+public class TemporaryDirectory(string DirectoryPath) : IDisposable
+{
+    public string DirectoryPath { get; } = DirectoryPath;
+
+    public void Dispose()
+    {
+        GC.SuppressFinalize(this);
+
+        try
+        {
+            Directory.Delete(DirectoryPath, true);
+        }
+        catch (Exception)
+        {
+            // Ignore
+        }
+    }
+
+    public static TemporaryDirectory Create()
+    {
+        var path = Path.Combine(Path.GetTempPath(), Guid.NewGuid().ToString("N"));
+        Directory.CreateDirectory(path);
+        return new TemporaryDirectory(path);
+    }
+}
