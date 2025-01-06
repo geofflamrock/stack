4198fc1e06c4e73a8a7b05be70a551dd3596bd76
Geoff Lamrock <geoff.lamrock@gmail.com>
2025-01-06 22:17:19 +1100

    Add happy path test

--- src/Stack.Tests/Git/GitClientTests.cs
+++ src/Stack.Tests/Git/GitClientTests.cs
@@ -41,4 +41,42 @@ public class GitClientTests(ITestOutputHelper testOutputHelper)
         // Assert
         merge.Should().Throw<MergeConflictException>();
     }
+
+    [Fact]
+    public void MergeFromLocalSourceBranch_WhenConflictsDoNotOccur_DoesNotThrow()
+    {
+        // Arrange
+        var branch1 = Some.BranchName();
+        var branch2 = Some.BranchName();
+        using var repo = new TestGitRepositoryBuilder()
+            .WithBranch(builder => builder.WithName(branch1))
+            .WithBranch(builder => builder.WithName(branch2).FromSourceBranch(branch1))
+            .Build();
+
+        var outputProvider = new TestOutputProvider(testOutputHelper);
+        var gitClient = new GitClient(outputProvider, repo.GitClientSettings);
+
+        var relativeFilePath1 = Some.Name();
+        var filePath1 = Path.Join(repo.LocalDirectoryPath, relativeFilePath1);
+
+        gitClient.ChangeBranch(branch1);
+        File.WriteAllText(filePath1, Some.Name());
+        repo.Stage(relativeFilePath1);
+        repo.Commit();
+
+        var relativeFilePath2 = Some.Name();
+        var filePath2 = Path.Join(repo.LocalDirectoryPath, relativeFilePath2);
+
+        gitClient.ChangeBranch(branch2);
+        File.WriteAllText(filePath2, Some.Name());
+        repo.Stage(relativeFilePath2);
+        repo.Commit();
+
+        // Act
+        gitClient.ChangeBranch(branch1);
+        var merge = () => gitClient.MergeFromLocalSourceBranch(branch2);
+
+        // Assert
+        merge.Should().NotThrow();
+    }
 }
