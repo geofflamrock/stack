5dc47ed066cd42116004549c93b56ac27ff4b232
Geoff Lamrock <geoff.lamrock@gmail.com>
2025-03-11 08:59:40 +1100

    Adds `--json` output formatting (#225)

--- src/Stack/Infrastructure/Commands/CommandWithOutput.cs
+++ src/Stack/Infrastructure/Commands/CommandWithOutput.cs
@@ -1,12 +1,18 @@
+using System.Text.Json;
 using Spectre.Console;
 using Spectre.Console.Cli;
 
 namespace Stack.Commands;
 
 public abstract class CommandWithOutput<TSettings, TResponse> : Command<TSettings>
-    where TSettings : CommandSettingsBase
+    where TSettings : CommandWithOutputSettingsBase
     where TResponse : notnull
 {
+    readonly JsonSerializerOptions jsonSerializerOptions = new()
+    {
+        PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+    };
+
     public override async Task<int> ExecuteAsync(CommandContext context, TSettings settings)
     {
         var response = await Execute(settings);
@@ -17,6 +23,24 @@ public abstract class CommandWithOutput<TSettings, TResponse> : Command<TSetting
 
     protected override abstract Task<TResponse> Execute(TSettings settings);
 
-    protected abstract void WriteOutput(TSettings settings, TResponse response);
+    protected abstract void WriteDefaultOutput(TResponse response);
+
+    protected virtual void WriteJsonOutput(TResponse response, JsonSerializerOptions options)
+    {
+        var json = JsonSerializer.Serialize(response, jsonSerializerOptions);
+        StdOut.WriteLine(json);
+    }
+
+    private void WriteOutput(TSettings settings, TResponse response)
+    {
+        if (settings.Json)
+        {
+            WriteJsonOutput(response, jsonSerializerOptions);
+        }
+        else
+        {
+            WriteDefaultOutput(response);
+        }
+    }
 }
 
