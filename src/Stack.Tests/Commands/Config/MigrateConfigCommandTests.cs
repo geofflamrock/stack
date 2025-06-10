352ec5dddb89fe537afd9acf67a53de84809ab1d
Geoff Lamrock <geoff.lamrock@gmail.com>
2025-06-11 08:24:38 +1000

    Improve tests

--- src/Stack.Tests/Commands/Config/MigrateConfigCommandTests.cs
+++ src/Stack.Tests/Commands/Config/MigrateConfigCommandTests.cs
@@ -135,5 +135,35 @@ namespace Stack.Tests.Commands
 
             inputProvider.DidNotReceive().Confirm(Questions.ConfirmMigrateConfig);
         }
+
+        [Fact]
+        public async Task WhenSchemaIsV2_DoesNotPerformMigration()
+        {
+            // Arrange
+            using var temporaryDirectory = TemporaryDirectory.Create();
+            var fileStackConfig = new FileStackConfig(temporaryDirectory.DirectoryPath);
+            var configPath = fileStackConfig.GetConfigPath();
+
+            var stack1 = new TestStackBuilder()
+                .WithBranch(b => b.WithName("branch-1").WithChildBranch(b => b.WithName("branch-2")))
+                .Build();
+
+            var stack2 = new TestStackBuilder()
+                .WithBranch(b => b.WithName("branch-3").WithChildBranch(b => b.WithName("branch-4")))
+                .Build();
+
+            fileStackConfig.Save(new StackData(SchemaVersion.V2, [stack1, stack2]));
+
+            var inputProvider = Substitute.For<IInputProvider>();
+            var logger = new TestLogger(testOutputHelper);
+            var handler = new MigrateConfigCommandHandler(inputProvider, logger, fileStackConfig);
+
+            // Act
+            await handler.Handle(new MigrateConfigCommandInputs(false));
+
+            // Assert
+            var migratedData = fileStackConfig.Load();
+            migratedData.Should().BeEquivalentTo(new StackData(SchemaVersion.V2, [stack1, stack2]));
+        }
     }
 }
