079761fdbfe19fe70ef2bd2483eaa7d3b2a84527
Geoff Lamrock <geoff.lamrock@gmail.com>
2025-08-05 19:52:46 +1000

    Add test for PullChanges to ensure only existing remote branches are pulled

--- src/Stack.Tests/Commands/Helpers/StackActionsTests.cs
+++ src/Stack.Tests/Commands/Helpers/StackActionsTests.cs
@@ -668,4 +668,45 @@ public class StackActionsTests(ITestOutputHelper testOutputHelper)
         gitClient.Received().PullBranch(branchWithRemoteChanges);
         gitClient.DidNotReceive().PullBranch(branchWithoutRemoteChanges);
     }
+
+    [Fact]
+    public void PullChanges_WhenSomeBranchesDoNotExistInRemote_OnlyPullsBranchesThatExistInRemote()
+    {
+        // Arrange
+        var sourceBranch = Some.BranchName();
+        var branchThatExistsInRemote = Some.BranchName();
+        var branchThatDoesNotExistInRemote = Some.BranchName();
+
+        var gitClient = Substitute.For<IGitClient>();
+
+        var branchStatus = new Dictionary<string, GitBranchStatus>
+        {
+            { sourceBranch, new GitBranchStatus(sourceBranch, $"origin/{sourceBranch}", true, false, 0, 0, new Commit(Some.Sha(), Some.Name())) },
+            { branchThatExistsInRemote, new GitBranchStatus(branchThatExistsInRemote, $"origin/{branchThatExistsInRemote}", true, false, 0, 2, new Commit(Some.Sha(), Some.Name())) },
+            { branchThatDoesNotExistInRemote, new GitBranchStatus(branchThatDoesNotExistInRemote, $"origin/{branchThatDoesNotExistInRemote}", false, false, 0, 0, new Commit(Some.Sha(), Some.Name())) }
+        };
+
+        gitClient.GetBranchStatuses(Arg.Any<string[]>()).Returns(branchStatus);
+
+        var stack = new TestStackBuilder()
+            .WithSourceBranch(sourceBranch)
+            .WithBranch(b => b.WithName(branchThatExistsInRemote))
+            .WithBranch(b => b.WithName(branchThatDoesNotExistInRemote))
+            .Build();
+
+        var stackActions = new StackActions(
+            gitClient,
+            Substitute.For<IGitHubClient>(),
+            Substitute.For<IInputProvider>(),
+            new TestLogger(testOutputHelper)
+        );
+
+        // Act
+        stackActions.PullChanges(stack);
+
+        // Assert
+        gitClient.DidNotReceive().PullBranch(sourceBranch);
+        gitClient.Received().PullBranch(branchThatExistsInRemote);
+        gitClient.DidNotReceive().PullBranch(branchThatDoesNotExistInRemote);
+    }
 }
