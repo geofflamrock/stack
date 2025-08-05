8a9c5981292b4a9007a91c890421dc8d728c8a9a
Geoff Lamrock <geoff.lamrock@gmail.com>
2025-08-05 20:02:29 +1000

    Add tests for PushChanges to verify behavior with new branches and batch sizes

--- src/Stack.Tests/Commands/Helpers/StackActionsTests.cs
+++ src/Stack.Tests/Commands/Helpers/StackActionsTests.cs
@@ -754,4 +754,182 @@ public class StackActionsTests(ITestOutputHelper testOutputHelper)
         // Assert
         branchesPushedToRemote.ToArray().Should().BeEquivalentTo([branchThatExistsInRemoteAndIsAhead]);
     }
+
+    [Fact]
+    public void PushChanges_WhenSomeBranchesHaveNoRemoteTrackingBranch_PushesThemAsNewBranches()
+    {
+        // Arrange
+        var sourceBranch = Some.BranchName();
+        var existingBranchAhead = Some.BranchName();
+        var newBranchWithNoRemote = Some.BranchName();
+
+        var gitClient = Substitute.For<IGitClient>();
+
+        var branchStatus = new Dictionary<string, GitBranchStatus>
+        {
+            { sourceBranch, new GitBranchStatus(sourceBranch, $"origin/{sourceBranch}", true, false, 0, 0, new Commit(Some.Sha(), Some.Name())) },
+            { existingBranchAhead, new GitBranchStatus(existingBranchAhead, $"origin/{existingBranchAhead}", true, false, 2, 0, new Commit(Some.Sha(), Some.Name())) },
+            { newBranchWithNoRemote, new GitBranchStatus(newBranchWithNoRemote, null, false, false, 0, 0, new Commit(Some.Sha(), Some.Name())) }
+        };
+
+        gitClient.GetBranchStatuses(Arg.Any<string[]>()).Returns(branchStatus);
+
+        var branchesPushedToRemote = new List<string>();
+        var newBranchesPushed = new List<string>();
+
+        gitClient
+            .WhenForAnyArgs(g => g.PushBranches(Arg.Any<string[]>(), Arg.Any<bool>()))
+            .Do((c) => branchesPushedToRemote.AddRange(c.Arg<string[]>()));
+
+        gitClient
+            .WhenForAnyArgs(g => g.PushNewBranch(Arg.Any<string>()))
+            .Do((c) => newBranchesPushed.Add(c.Arg<string>()));
+
+        var stack = new TestStackBuilder()
+            .WithSourceBranch(sourceBranch)
+            .WithBranch(b => b.WithName(existingBranchAhead))
+            .WithBranch(b => b.WithName(newBranchWithNoRemote))
+            .Build();
+
+        var stackActions = new StackActions(
+            gitClient,
+            Substitute.For<IGitHubClient>(),
+            Substitute.For<IInputProvider>(),
+            new TestLogger(testOutputHelper)
+        );
+
+        // Act
+        stackActions.PushChanges(stack, 5, false);
+
+        // Assert
+        newBranchesPushed.Should().BeEquivalentTo([newBranchWithNoRemote]);
+        branchesPushedToRemote.Should().BeEquivalentTo([existingBranchAhead]);
+    }
+
+    [Fact]
+    public void PushChanges_WhenMaxBatchSizeIsSmaller_PushesBranchesInMultipleBatches()
+    {
+        // Arrange
+        var sourceBranch = Some.BranchName();
+        var branch1 = Some.BranchName();
+        var branch2 = Some.BranchName();
+        var branch3 = Some.BranchName();
+
+        var gitClient = Substitute.For<IGitClient>();
+
+        var branchStatus = new Dictionary<string, GitBranchStatus>
+        {
+            { sourceBranch, new GitBranchStatus(sourceBranch, $"origin/{sourceBranch}", true, false, 0, 0, new Commit(Some.Sha(), Some.Name())) },
+            { branch1, new GitBranchStatus(branch1, $"origin/{branch1}", true, false, 1, 0, new Commit(Some.Sha(), Some.Name())) },
+            { branch2, new GitBranchStatus(branch2, $"origin/{branch2}", true, false, 2, 0, new Commit(Some.Sha(), Some.Name())) },
+            { branch3, new GitBranchStatus(branch3, $"origin/{branch3}", true, false, 1, 0, new Commit(Some.Sha(), Some.Name())) }
+        };
+
+        gitClient.GetBranchStatuses(Arg.Any<string[]>()).Returns(branchStatus);
+
+        var pushCalls = new List<List<string>>();
+
+        gitClient
+            .WhenForAnyArgs(g => g.PushBranches(Arg.Any<string[]>(), Arg.Any<bool>()))
+            .Do((c) => pushCalls.Add(c.Arg<string[]>().ToList()));
+
+        var stack = new TestStackBuilder()
+            .WithSourceBranch(sourceBranch)
+            .WithBranch(b => b.WithName(branch1))
+            .WithBranch(b => b.WithName(branch2))
+            .WithBranch(b => b.WithName(branch3))
+            .Build();
+
+        var stackActions = new StackActions(
+            gitClient,
+            Substitute.For<IGitHubClient>(),
+            Substitute.For<IInputProvider>(),
+            new TestLogger(testOutputHelper)
+        );
+
+        // Act
+        stackActions.PushChanges(stack, maxBatchSize: 2, forceWithLease: false);
+
+        // Assert
+        pushCalls.Should().HaveCount(2);
+        pushCalls[0].Should().HaveCount(2);
+        pushCalls[1].Should().HaveCount(1);
+        pushCalls.SelectMany(batch => batch).Should().BeEquivalentTo([branch1, branch2, branch3]);
+    }
+
+    [Fact]
+    public void PushChanges_WhenForceWithLeaseIsTrue_PassesForceWithLeaseParameterToPushBranches()
+    {
+        // Arrange
+        var sourceBranch = Some.BranchName();
+        var branchAhead = Some.BranchName();
+
+        var gitClient = Substitute.For<IGitClient>();
+
+        var branchStatus = new Dictionary<string, GitBranchStatus>
+        {
+            { sourceBranch, new GitBranchStatus(sourceBranch, $"origin/{sourceBranch}", true, false, 0, 0, new Commit(Some.Sha(), Some.Name())) },
+            { branchAhead, new GitBranchStatus(branchAhead, $"origin/{branchAhead}", true, false, 1, 0, new Commit(Some.Sha(), Some.Name())) }
+        };
+
+        gitClient.GetBranchStatuses(Arg.Any<string[]>()).Returns(branchStatus);
+
+        var stack = new TestStackBuilder()
+            .WithSourceBranch(sourceBranch)
+            .WithBranch(b => b.WithName(branchAhead))
+            .Build();
+
+        var stackActions = new StackActions(
+            gitClient,
+            Substitute.For<IGitHubClient>(),
+            Substitute.For<IInputProvider>(),
+            new TestLogger(testOutputHelper)
+        );
+
+        // Act
+        stackActions.PushChanges(stack, maxBatchSize: 5, forceWithLease: true);
+
+        // Assert
+        gitClient.Received().PushBranches(Arg.Is<string[]>(branches => branches.Contains(branchAhead)), true);
+    }
+
+    [Fact]
+    public void PushChanges_WhenNoBranchesNeedToBePushed_DoesNotCallPushMethods()
+    {
+        // Arrange
+        var sourceBranch = Some.BranchName();
+        var branchUpToDate = Some.BranchName();
+        var branchBehind = Some.BranchName();
+
+        var gitClient = Substitute.For<IGitClient>();
+
+        var branchStatus = new Dictionary<string, GitBranchStatus>
+        {
+            { sourceBranch, new GitBranchStatus(sourceBranch, $"origin/{sourceBranch}", true, false, 0, 0, new Commit(Some.Sha(), Some.Name())) },
+            { branchUpToDate, new GitBranchStatus(branchUpToDate, $"origin/{branchUpToDate}", true, false, 0, 0, new Commit(Some.Sha(), Some.Name())) },
+            { branchBehind, new GitBranchStatus(branchBehind, $"origin/{branchBehind}", true, false, 0, 2, new Commit(Some.Sha(), Some.Name())) }
+        };
+
+        gitClient.GetBranchStatuses(Arg.Any<string[]>()).Returns(branchStatus);
+
+        var stack = new TestStackBuilder()
+            .WithSourceBranch(sourceBranch)
+            .WithBranch(b => b.WithName(branchUpToDate))
+            .WithBranch(b => b.WithName(branchBehind))
+            .Build();
+
+        var stackActions = new StackActions(
+            gitClient,
+            Substitute.For<IGitHubClient>(),
+            Substitute.For<IInputProvider>(),
+            new TestLogger(testOutputHelper)
+        );
+
+        // Act
+        stackActions.PushChanges(stack, maxBatchSize: 5, forceWithLease: false);
+
+        // Assert
+        gitClient.DidNotReceive().PushBranches(Arg.Any<string[]>(), Arg.Any<bool>());
+        gitClient.DidNotReceive().PushNewBranch(Arg.Any<string>());
+    }
 }
