081e6dc217a9e207a5f6101f38edf9552206832a
Geoff Lamrock <geoff.lamrock@gmail.com>
2025-08-05 19:55:08 +1000

    Add test for PushChanges to ensure only existing remote branches are pushed

--- src/Stack.Tests/Commands/Helpers/StackActionsTests.cs
+++ src/Stack.Tests/Commands/Helpers/StackActionsTests.cs
@@ -709,4 +709,49 @@ public class StackActionsTests(ITestOutputHelper testOutputHelper)
         gitClient.Received().PullBranch(branchThatExistsInRemote);
         gitClient.DidNotReceive().PullBranch(branchThatDoesNotExistInRemote);
     }
+
+    [Fact]
+    public void PushChanges_WhenSomeBranchesDoNotExistInRemote_OnlyPushesBranchesThatExistInRemote()
+    {
+        // Arrange
+        var sourceBranch = Some.BranchName();
+        var branchThatExistsInRemoteAndIsAhead = Some.BranchName();
+        var branchThatDoesNotExistInRemoteButIsAhead = Some.BranchName();
+
+        var gitClient = Substitute.For<IGitClient>();
+
+        var branchStatus = new Dictionary<string, GitBranchStatus>
+        {
+            { sourceBranch, new GitBranchStatus(sourceBranch, $"origin/{sourceBranch}", true, false, 0, 0, new Commit(Some.Sha(), Some.Name())) },
+            { branchThatExistsInRemoteAndIsAhead, new GitBranchStatus(branchThatExistsInRemoteAndIsAhead, $"origin/{branchThatExistsInRemoteAndIsAhead}", true, false, 2, 0, new Commit(Some.Sha(), Some.Name())) },
+            { branchThatDoesNotExistInRemoteButIsAhead, new GitBranchStatus(branchThatDoesNotExistInRemoteButIsAhead, $"origin/{branchThatDoesNotExistInRemoteButIsAhead}", false, false, 2, 0, new Commit(Some.Sha(), Some.Name())) }
+        };
+
+        gitClient.GetBranchStatuses(Arg.Any<string[]>()).Returns(branchStatus);
+
+        var branchesPushedToRemote = new List<string>();
+
+        gitClient
+            .WhenForAnyArgs(g => g.PushBranches(Arg.Any<string[]>(), Arg.Any<bool>()))
+            .Do((c) => branchesPushedToRemote.AddRange(c.Arg<string[]>()));
+
+        var stack = new TestStackBuilder()
+            .WithSourceBranch(sourceBranch)
+            .WithBranch(b => b.WithName(branchThatExistsInRemoteAndIsAhead))
+            .WithBranch(b => b.WithName(branchThatDoesNotExistInRemoteButIsAhead))
+            .Build();
+
+        var stackActions = new StackActions(
+            gitClient,
+            Substitute.For<IGitHubClient>(),
+            Substitute.For<IInputProvider>(),
+            new TestLogger(testOutputHelper)
+        );
+
+        // Act
+        stackActions.PushChanges(stack, 5, false);
+
+        // Assert
+        branchesPushedToRemote.ToArray().Should().BeEquivalentTo([branchThatExistsInRemoteAndIsAhead]);
+    }
 }
