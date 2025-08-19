da10153810d6806f3253d3d323c678c6a70681d9
Geoff Lamrock <geoff.lamrock@gmail.com>
2025-08-19 20:04:56 +1000

    Move PushChanges to improve testing (#327)

--- src/Stack/Commands/Helpers/StackActions.cs
+++ src/Stack/Commands/Helpers/StackActions.cs
@@ -7,6 +7,7 @@ namespace Stack.Commands.Helpers
     public interface IStackActions
     {
         void PullChanges(Config.Stack stack);
+        void PushChanges(Config.Stack stack, int maxBatchSize, bool forceWithLease);
     }
 
     public class StackActions(IGitClient gitClient, ILogger logger) : IStackActions
@@ -25,6 +26,43 @@ namespace Stack.Commands.Helpers
                 logger.Information($"Pulling changes for {branch.Branch()} from remote");
                 gitClient.ChangeBranch(branch);
                 gitClient.PullBranch(branch);
+            }
+        }
+
+        public void PushChanges(
+            Config.Stack stack,
+            int maxBatchSize,
+            bool forceWithLease)
+        {
+            var branchStatus = gitClient.GetBranchStatuses([.. stack.AllBranchNames]);
+
+            var branchesThatHaveNotBeenPushedToRemote = branchStatus
+                .Where(b => b.Value.RemoteTrackingBranchName is null)
+                .Select(b => b.Value.BranchName)
+                .ToList();
+
+            foreach (var branch in branchesThatHaveNotBeenPushedToRemote)
+            {
+                logger.Information($"Pushing new branch {branch.Branch()} to remote");
+                gitClient.PushNewBranch(branch);
+            }
+
+            var branchesThatAreAheadOfTheRemote = branchStatus
+                .Where(b => b.Value.RemoteBranchExists && b.Value.Ahead > 0)
+                .Select(b => b.Value.BranchName)
+                .ToList();
+
+            var branchGroupsToPush = branchesThatAreAheadOfTheRemote
+                .Select((b, i) => new { Index = i, Value = b })
+                .GroupBy(b => b.Index / maxBatchSize)
+                .Select(g => g.Select(b => b.Value).ToList())
+                .ToList();
+
+            foreach (var branches in branchGroupsToPush)
+            {
+                logger.Information($"Pushing changes for {string.Join(", ", branches.Select(b => b.Branch()))} to remote");
+
+                gitClient.PushBranches([.. branches], forceWithLease);
             }
         }
     }
